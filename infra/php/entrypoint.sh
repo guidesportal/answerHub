af46c6de93a94dc4d82be8bfc162d395d5a0d3ac
faguilarble <f.aguilar.ble@gmail.com>
2025-08-20 16:35:50 +0200

    Do not install when building, run composer install on entrypoint for the first time

--- infra/php/entrypoint.sh
+++ infra/php/entrypoint.sh
@@ -3,23 +3,23 @@ set -e
 cleanup() {
     echo "Running shutdown tasks..."
     php artisan cache:clear
-    php artisan migrate:rollback
     rm /home/app/.migrated
     rm /home/app/.seeded
+    rm /home/app/.installed
 }
 trap cleanup SIGTERM
 if [ ! -f /home/app/.env ]; then
     cp /home/app/.env.example /home/app/.env
 fi
+if [ ! -f /home/app/.installed ]; then
+    composer install --no-interaction --prefer-dist && touch /home/app/.installed
+fi
 if [ ! -f /home/app/.migrated ]; then
-    php artisan migrate --force
-    touch /home/app/.migrated
+    php artisan migrate:fresh --force && touch /home/app/.migrated
 fi
 if [ ! -f /home/app/.seeded ]; then
-    php artisan db:seed --class=CreateTestData --force
-    touch /home/app/.seeded
+    php artisan db:seed --class=CreateTestData --force && touch /home/app/.seeded
 fi
-
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
         set -- php "$@"
